15fcbd7ffe5506b94bdc86eed21a01173376f8de
Marcos Matheus <mateus312004@gmail.com>
2025-05-07 11:08:38 -0300

    Remover pasta aulas do repositório

--- aulas/aula1.js
+++
@@ -1,3 +0,0 @@
-//console.log("Esta linha está comentada");
-console.log("alô Mundo");
-console.log("Fim");
--- aulas/aula2.js
+++
@@ -1,6 +0,0 @@
-"use strict"; //modo estrtito
-
-let nome = "Matheus";
-console.log("Curso de Javascript");
-console.log(nome);
-console.log("nome: " + nome);
